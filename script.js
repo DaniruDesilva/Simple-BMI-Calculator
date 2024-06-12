f9660696b0660eeb899c291b506c37915ecd53fc
Daniru De Silva <165991056+DaniruDesilva@users.noreply.github.com>
2024-06-12 23:39:17 +0530

    Simple BMI Calculator

---
+++ script.js
@@ -0,0 +1,8 @@
+function calculate(){
+    let weight = document.getElementById('weight').value;
+    let height = document.getElementById('height').value;
+    let bmiweight = document.getElementsByClassName('weight');
+    let bmiheight = document.getElementsByClassName('height');
+
+    
+}
